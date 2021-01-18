40ccc27bb13768f490138e629e456e1354b9240c
Alex Pacheco <alexrodpacheco@gmail.com>
2021-01-17 22:01:29 -0300

    TaskCell and workigng with ViewController

---
+++ to_do_TaskListApp/TaskCell.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCell.swift
+//  to_do_TaskListApp
+//
+//  Created by Alex Rodrigues Pacheco on 17/01/21.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    
+    @IBOutlet weak var checkedBoxOutlet: UIButton!
+    @IBOutlet weak var taskNameLabel: UILabel!
+    @IBAction func taskButtonPressed(_ sender: Any) {
+    }
+    
+}
--- to_do_TaskListApp/ViewController.swift
+++ to_do_TaskListApp/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tasks.append(Task(name: "Test"))
+       
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskCell
+        
+        cell.taskNameLabel.text = tasks[indexPath.row].name
+        
+        if tasks[indexPath.row].check {
+            cell.checkedBoxOutlet.setBackgroundImage(#imageLiteral(resourceName: "checkBoxFILLED"), for: UIControl.State.normal)
+        } else {
+            cell.checkedBoxOutlet.setBackgroundImage(#imageLiteral(resourceName: "checkBoxOUTLINE"), for: UIControl.State.normal)
+        }
+        
+        
+        return cell
+    }
+    
+    class Task {
+        var name: String = ""
+        var check: Bool = false
+        
+        convenience init(name: String) {
+            self.init()
+            self.name = name
+        }
+    }
 
 }
 
